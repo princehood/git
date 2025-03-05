ba88cdda6774af276a53d6922f331291c741aaae
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-03-05 23:14:51 +0100

    fixup??? Introduce helper to create symlinks that knows about index_state
    And another merge conflict resolution.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw-posix.h
+++ compat/mingw-posix.h
@@ -195,6 +195,9 @@ int sigaction(int sig, struct sigaction *in, struct sigaction *out);
 int link(const char *oldpath, const char *newpath);
 int uname(struct utsname *buf);
 int readlink(const char *path, char *buf, size_t bufsiz);
+struct index_state;
+int mingw_create_symlink(struct index_state *index, const char *target, const char *link);
+#define create_symlink mingw_create_symlink
 
 /*
  * replacements of existing functions
